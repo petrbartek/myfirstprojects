b8513a8d27fffd9e5717352a6f098e1a30c4b290
1319473 <1319473@rgu.ac.uk>
2017-03-07 12:13:48 +0000

    me

--- week6/intro.php
+++ week6/intro.php
@@ -5,8 +5,19 @@
 <body>
 <p>
     <?php
-    echo "Hello World";
-    echo "Hello," . " " . "world" . "!";
+   $myage >= "16";
+   $myage >= "18";
+   $myage >= "21";
+
+    if($myage >= "16"){
+        print "specs";
+    }
+    elseif($myage >="18"){
+        print "mugs";
+    }
+    elseif($myage >= "21"){
+        print "susagerolls";
+    }
     ?>
 </p>
 </body>
