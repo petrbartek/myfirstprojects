4e4297f6fe5e7d3cd3e335571e8a169a40e5da4f
1319473 <1319473@rgu.ac.uk>
2017-03-07 13:35:02 +0000

    me

--- week6/array.php
+++ week6/array.php
@@ -5,13 +5,17 @@
 <body>
 <p>
     <?php
-    $myArray=array("do", "re","mi");//declares tha array
+    $provisionedActivity=array("specs","mugs","sausagerolls");//declares the array
 
-    echo $myArray[0]//outputs "do"
+    echo $provisionedActivity[0]; //outputs "specs"
 
-    $myArray[1]="la";//modifies position 1(re)
+    $provisionedActivity[1]= "hugs";//changes mugs to hugs
 
-    unset($array[2]);//removes the array in position 2
+    unset($provisionedActivity[2]);//removes susagerolls
+
+foreach($provisionedActivity as $x){
+        print "<p>$x</ p>";
+ }
     ?>
 </p>
 </body>
