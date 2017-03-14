cc416e8c24e74f747d89f1eade1782a7315a3ed3
1319473 <1319473@rgu.ac.uk>
2017-03-14 15:12:54 +0000

    me

---
+++ week5/function.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1319473
+ * Date: 14/03/2017
+ * Time: 14:52
+ */
+
+$people = array("petr", "ashley", "steve", "will", "coco", "tukan", "bart", "les");
+sort($people);
+print rand($people);
+
+
+
+
+?>
--- week5/whileloop.php
+++ week5/whileloop.php
@@ -1,10 +1,10 @@
 <?php
-$items = array ("specs", "mugs", "sausages");
+$items = array("specs", "mugs", "sausages");
 
 echo "for sale today: $items <br>";
 
 do {
-    $items = rand(0, 1, 2);
+    $items = rand(0,1,2);
     $items = 7; $day = 1; $day <= 30; $day++;
     if ($items = 0){
         echo "the good will be available next month again";
