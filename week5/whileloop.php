4dd5bf5cfbe1ef17b75fef078ba7dfbd6e68b04b
1319473 <1319473@rgu.ac.uk>
2017-03-14 14:51:43 +0000

    me

--- week5/whileloop.php
+++ week5/whileloop.php
@@ -1,5 +1,5 @@
 <?php
-$itemsitems = array ("specs", "mugs", "sausages");
+$items = array ("specs", "mugs", "sausages");
 
 echo "for sale today: $items <br>";
 
@@ -7,7 +7,7 @@ do {
     $items = rand(0, 1, 2);
     $items = 7; $day = 1; $day <= 30; $day++;
     if ($items = 0){
-        echo "the good will be available next menth again";
+        echo "the good will be available next month again";
     }
 
 
