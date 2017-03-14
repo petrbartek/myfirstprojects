a5afc72171fb9276c69b17dbe427fcc15295ef59
1319473 <1319473@rgu.ac.uk>
2017-03-14 14:35:40 +0000

    me

--- week5/whileloop.php
+++ week5/whileloop.php
@@ -3,9 +3,11 @@
 $items = array ("specs", "mugs", "sausages");
 do {
     echo "for sale today: $items <br>";
-    $day = 1; $day <=30; $day++;
-    $items = rand(0,1,2);
-    while ($items <=7);
+    $day = 1;
+    $day <= 30;
+    $day++;
+    $items = rand(0, 1, 2);
+}while ($items <=7);
        
-    }
+
 ?>
