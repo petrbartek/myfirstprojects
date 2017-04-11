55d3ccf4d115ebe004b9cf0ecf435f6709d7eb95
petrbartek <petrbartek@hotmail.co.uk>
2017-04-11 13:57:18 +0100

    Update mysqli_connect.php

--- admintable/mysqli_connect.php
+++ admintable/mysqli_connect.php
@@ -1,14 +1,22 @@
-<?php 
-// This creates a connection to the admintable database and to MySQL, 
-// It also sets the encoding.
-// Set the access details as constants:
-DEFINE ('DB_USER', 'azure');
-DEFINE ('DB_PASSWORD', 'graduaceRGU@2016');
-DEFINE ('DB_HOST', 'localhost');
-DEFINE ('DB_NAME', 'admintable');
-
-// Make the connection:
-$dbcon = @mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die ('Could not connect to MySQL: ' . mysqli_connect_error() );
-
-// Set the encoding...
-mysqli_set_charset($dbcon, 'utf8');
+<?
+$connectstr_dbhost = '';
+$connectstr_dbname = '';
+$connectstr_dbusername = '';
+$connectstr_dbpassword = '';
+foreach ($_SERVER as $key => $value) {
+ if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+ continue;
+ }
+ $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+ $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+ $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+ $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+}
+$link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword, $connectstr_dbname);
+if (!$link) {
+ echo "Error: Unable to connect to MySQL." . PHP_EOL;
+ echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+ echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+ exit;
+}
+?>
