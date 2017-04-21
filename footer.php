dc332d1176994941285f329855c3ced374205765
petrbartek <petrbartek@hotmail.co.uk>
2017-04-21 15:04:53 +0100

    Delete footer.php

--- footer.php
+++
@@ -1,8 +0,0 @@
-<style type="text/css">
-#footer { clear:both; margin:auto; height: 19px; text-align:center;
-}
-</style>
-<div id="footer">
-		<p>Copyright &copy; Petr Bartek | Designed by <a href="http://www.google.co.uk/">Petr Bartek</a> | Valid <a href="http://jigsaw.w3.org/css-validator/">CSS</a> &amp; 
-		<a href="http://validator.w3.org/">HTML5</a></p>
-</div>
